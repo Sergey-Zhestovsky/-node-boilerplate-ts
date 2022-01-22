47f1d36210f26b7c79a57f9932b1dd3037d8a174
Sergey Zhestovsky <sergey.zhestovsky@gmail.com>
2022-01-22 04:02:53 +0200

    adjust EventBus

--- src/libs/event-bus/EventBus.ts
+++ src/libs/event-bus/EventBus.ts
@@ -1,29 +1,49 @@
 import EventEmitter from 'events';
 
+import { HealthManager } from '../health-manager';
 import { BaseEvent } from './BaseEvent';
 import { IBusEvent, TListener } from './types';
 
 class EventBus {
+  private static getEventName(eventClass: IBusEvent | string) {
+    return typeof eventClass === 'function' ? eventClass.EventName : eventClass;
+  }
+
   private readonly eventEmitter = new EventEmitter();
 
-  on<T extends BaseEvent>(eventClass: IBusEvent<T> | string, listener: TListener<T>) {
-    const eventName = typeof eventClass === 'function' ? eventClass.EventName : eventClass;
-    this.eventEmitter.on(eventName, listener);
+  on<T extends BaseEvent>(event: MaybeArray<IBusEvent<T> | string>, listener: TListener<T>) {
+    const events = Array.isArray(event) ? event : [event];
+
+    for (const e of events) {
+      this.eventEmitter.on(EventBus.getEventName(e), listener);
+    }
   }
 
-  one<T extends BaseEvent>(eventClass: IBusEvent<T> | string, listener: TListener<T>) {
-    const eventName = typeof eventClass === 'function' ? eventClass.EventName : eventClass;
-    this.eventEmitter.once(eventName, listener);
+  one<T extends BaseEvent>(event: MaybeArray<IBusEvent<T> | string>, listener: TListener<T>) {
+    const events = Array.isArray(event) ? event : [event];
+
+    for (const e of events) {
+      this.eventEmitter.once(EventBus.getEventName(e), listener);
+    }
   }
 
-  off<T extends BaseEvent>(eventClass: IBusEvent<T> | string, listener: TListener<T>) {
-    const eventName = typeof eventClass === 'function' ? eventClass.EventName : eventClass;
-    this.eventEmitter.off(eventName, listener);
+  off<T extends BaseEvent>(event: MaybeArray<IBusEvent<T> | string>, listener: TListener<T>) {
+    const events = Array.isArray(event) ? event : [event];
+
+    for (const e of events) {
+      this.eventEmitter.off(EventBus.getEventName(e), listener);
+    }
   }
 
   emit(event: BaseEvent) {
-    const eventName = event.EventName;
-    this.eventEmitter.emit(eventName, event);
+    Promise.resolve()
+      .then(() => {
+        const eventName = event.EventName;
+        this.eventEmitter.emit(eventName, event);
+      })
+      .catch((error) => {
+        HealthManager.report(error);
+      });
   }
 }
 
